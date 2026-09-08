8ae448c97e22145783fcdd1ff53fcc2e8bcb203a
lucianosparta <lucianosparta0@gmail.com>
2026-09-08 01:44:26 -0300

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('luciano-sparta-tp1-prog4-2026-c2');
+}
